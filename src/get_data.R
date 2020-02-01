4e9205d72f475695977399546e82e718dc3ee6c5
Karlos Muradyan <carlosmuradyan2@gmail.com>
2020-02-01 01:51:05 -0800

    Added documentation for get_data.R file

--- src/get_data.R
+++ src/get_data.R
@@ -35,6 +35,13 @@ main <- function () {
 }
 
 download_data <- function(url, dir, filepath) {
+	#` Downloads data from url and places in given path
+	#`
+	#` @param url string containing url of the file to be downloaded
+	#` @param dir folder path where the file should be placed. If path does not exist, it will be created
+        #` @param filepath full filepath. The same dir but with filename included
+	#`	
+	#` @return TRUE, if everything went without any problems, otherwise FALSE
 	return(tryCatch({
 		if(!dir.exists(dir)){
 			dir.create(dir, recursive = TRUE)
@@ -60,6 +67,12 @@ download_data <- function(url, dir, filepath) {
 }
 
 unzip_data <- function(filepath, out_dir) {
+	#` Unzipping file located in filepath to the directory mentioned
+	#`
+	#` @param filepath str containing full path of the file that should be unzipped
+        #` @param out_dir str containing path of the folder where the files should be extracted
+	#`	
+	#` @return TRUE, if everything went without any problems, otherwise FALSE
 	print(paste('Unzipping file: ', filepath))
 	return(tryCatch({
 		unzip(filepath, exdir=out_dir)
